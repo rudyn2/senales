f464d54958bdb0a82cd2bb9cbf8b2b05846802a6
Rudy Garcia Alvarado <c01010010@hotmail.com>
2018-11-11 18:28:39 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,1 +1,16 @@
-% Parte 1: 
+% Parte 1: Grafique la se?al en el tiempo y en frecuencia.
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+% Parte 2: Obtenga y grafique la se?al portadora en tiempo y frecuencia,
+% recuerde que se desea desplazar la se?al a 3 kHz.
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+% Parte 3: Demodule la se?al de la parte 2. Grafique en tiempo y
+% frecuencia. Escuche y compare la se?al obtenida con la original.
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
