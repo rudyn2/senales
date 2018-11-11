99e604ee19dddea7bcddf463973f218da8830811
Rudy Garcia Alvarado <c01010010@hotmail.com>
2018-11-11 18:31:32 -0300

    Update main.m

--- main.m
+++ main.m
@@ -12,5 +12,12 @@ pause;
 % Parte 3: Demodule la se?al de la parte 2. Grafique en tiempo y
 % frecuencia. Escuche y compare la se?al obtenida con la original.
 
+% Restricciones:
+% - Utilizar la misma frecuencia de modulaci?n.
+% - La atenuaci?n no debe ser menor a 30 dB. Las p?rdidas en la banda
+%   de paso no pueden ser mayores a 1 dB. Considere que la se?al se
+%   encuentra contenida en la bada de 2 kHz y que la frecuencia de corte se
+%   encuentra en +-1.2 kHz (con respecto a la frecuencia central).
+
 fprintf('Program paused. Press enter to continue.\n');
 pause;
