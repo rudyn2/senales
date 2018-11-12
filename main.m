e3b1c7713377278cca64fc2fbb311c25d854af4a
Rudy Garcia Alvarado <c01010010@hotmail.com>
2018-11-12 10:57:23 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,13 +1,39 @@
 % Parte 1: Grafique la se?al en el tiempo y en frecuencia.
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+filename = '/T5.wav';
+[y,Fs] = audioread(filename);
+L = size(y);
+T = 1/Fs;
+
+
+fprintf('Frecuencia de muestreo: %d\n', Fs);
+fprintf('Tiempo entre muestras : %d\n', T);
+t = 0:T:((L-1)*T);
+
+hold on
+title('Amplitud de la senal en el tiempo');
+xlabel('Tiempo [s]');
+ylabel('Amplitud [V]');
+%plot(t,y);
+
+
+F = fft(y);
+P2 = abs(F/L(1));
+P1 = P2(1:L(1)/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L(1)/2))/L(1);
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+
+
 
 % Parte 2: Obtenga y grafique la se?al portadora en tiempo y frecuencia,
 % recuerde que se desea desplazar la se?al a 3 kHz.
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+
 
 % Parte 3: Demodule la se?al de la parte 2. Grafique en tiempo y
 % frecuencia. Escuche y compare la se?al obtenida con la original.
@@ -18,6 +44,3 @@ pause;
 %   de paso no pueden ser mayores a 1 dB. Considere que la se?al se
 %   encuentra contenida en la bada de 2 kHz y que la frecuencia de corte se
 %   encuentra en +-1.2 kHz (con respecto a la frecuencia central).
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
